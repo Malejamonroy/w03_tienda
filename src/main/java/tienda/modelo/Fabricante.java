b970298fb07d8db6d06ac5470c36d67925c6668b
Malejamonroy <malejamonroyz@gmail.com>
2025-09-10 13:57:20 +0200

    json

--- src/main/java/tienda/modelo/Fabricante.java
+++ src/main/java/tienda/modelo/Fabricante.java
@@ -3,6 +3,8 @@ package tienda.modelo;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@JsonIgnoreProperties("productos")
 @Entity
 @Table(name = "fabricantes")
 public class Fabricante {
---
+++ src/test/java/tienda/json/TestJackson.java
@@ -0,0 +1,23 @@
+package tienda.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tienda.modelo.Producto;
+import tienda.persistencia.ProductoDao;
+import tienda.persistencia.ProductoDaoImpl;
+
+public class TestJackson {
+
+	public static void main(String[] args) throws JsonProcessingException {
+		ProductoDao pDao= new ProductoDaoImpl();
+		Producto p = pDao.findById(7);
+		System.out.println(p);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String json= mapper.writeValueAsString(p);
+		System.out.println(json);;
+	}
+
+}
